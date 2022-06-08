c6a1c24e90a80118e511fcebfaaf7e9635062490
torami <rami.torkhani@gmail.com>
2022-06-09 00:18:39 +0200

    add instructions for some classes

--- src/main/java/com/gkemayo/library/category/Category.java
+++ src/main/java/com/gkemayo/library/category/Category.java
@@ -1,4 +1,44 @@
 package com.gkemayo.library.category;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "CATEGORY")
 public class Category {
+
+    public Category() {
+    }
+
+    public Category(String code, String label) {
+        super();
+        this.code = code;
+        this.label = label;
+    }
+
+    private String code;
+
+    private String label;
+
+    @Id
+    @Column(name = "CODE")
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Column(name = "LABEL", nullable = false)
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
 }
+
+// + les méthodes hashCode() et equals() que vous retrouverez dans les sources de cet article
